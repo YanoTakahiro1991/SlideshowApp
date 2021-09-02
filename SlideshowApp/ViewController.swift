73349f5a0ff0d9190fa75afd87a1cd9dd71aad56
Takahiro Yano <after-schoolnana@i.softbank.jp>
2021-09-02 14:15:29 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Yano Takahiro  on 2021/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
